ee6ce64866a97680465e33708aa7f6a5f4ad94ae
dinuamipara03 <143832639+dinuamipara03@users.noreply.github.com>
2025-02-17 07:05:50 +0000

    commit it

---
+++ javascript/06_built_in_function.js
@@ -0,0 +1,27 @@
+//eval - Evaluates a string and returns a value.
+
+var x = 2
+var y = 39
+var z = "42"
+console.log(eval("x + y + 5")) 
+console.log(eval(x + y + 5)) 
+console.log(eval(z))
+
+//parseInt
+
+// console.log(parseInt("10"))
+// console.log(parseInt("10.00"))
+// console.log(parseInt("10.33"))
+// console.log(parseInt("34 45 66"))
+// console.log(parseInt("   60   "))
+// console.log(parseInt("40 years"))
+// console.log(parseInt("He was 40"))
+console.log(parseInt("0x11", 16))
+
+//parseFloat
+
+// console.log(parseFloat(10))
+// console.log(parseFloat("10"))
+// console.log(parseFloat("10.33"))
+// console.log(parseFloat("34 45 66"))
+// console.log(parseFloat("He was 40"))
