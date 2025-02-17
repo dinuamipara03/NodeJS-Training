8709a5dad5b4af5aa265ed4526b9e3a1b6513842
dinuamipara03 <143832639+dinuamipara03@users.noreply.github.com>
2025-02-17 05:03:42 +0000

    datatypes

---
+++ javascript/04_datatypes.js
@@ -0,0 +1,40 @@
+//string
+let string = "Dinaxi"
+console.log(string);
+console.log(typeof string);
+
+//number
+let number = 23
+console.log(number);
+console.log(typeof number);
+
+let floatNumber = 23.5
+console.log(floatNumber);
+console.log(typeof floatNumber);
+
+//boolean
+let boolean = true
+console.log(boolean);
+console.log(typeof boolean);
+
+//symbol
+const sym = Symbol("foo");
+console.log(sym);
+console.log(typeof sym);
+
+//object
+let myObj={
+    name:"Dinaxi",
+    course:"CE",
+}
+console.log(myObj);
+console.log(typeof myObj);
+
+//null
+console.log(typeof null);
+
+//undefined
+console.log(typeof undefined);
+
+
+
