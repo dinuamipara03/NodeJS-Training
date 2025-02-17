bb34191cf10a0dc1c15a98a9d9ccdb35d0245cad
dinuamipara03 <143832639+dinuamipara03@users.noreply.github.com>
2025-02-17 08:47:46 +0000

    date

--- javascript/05_methods.js
+++ javascript/05_methods.js
@@ -94,41 +94,6 @@ const person = {
 
     */
 
-  //++++++++++++++++++++++++++++++++++Date methods+++++++++++++++++++++++++++++++++
-/*
-
-  //++++get++++
-  const d = new Date()
-  // console.log(d)
-  // console.log(d.getDate())
-  // console.log(d.getDay())
-  // console.log(d.getFullYear())
-  // console.log(d.getHours())
-  // console.log(d.getMilliseconds())
-  // console.log(d.getMinutes())
-  // console.log(d.getMonth())
-  // console.log(d.getSeconds())
-  // console.log(d.getTime())
-  // console.log(d.getTimezoneOffset())
-  // console.log(d.getUTCDate())
-
-
-  //++++set++++
-  d.setDate(25)
-  console.log(d)
-  d.setFullYear(2002)
-  console.log(d)
-  d.setHours(23)
-  console.log(d)
-  d.setMinutes(35)
-  console.log(d)
-  d.setSeconds(99)
-  console.log(d)
-  d.setTime(3)
-  console.log(d)
-
-*/
-
 //+++++++++++++++++++++++++++++++++++++++set+++++++++++++++++++++++++++++++++++++
 /*
 const letters = new Set(["a","b","c"]);
