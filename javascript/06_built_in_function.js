9fe8a4eceecc59aac1a7d11c7496eb1352fe16aa
dinuamipara03 <143832639+dinuamipara03@users.noreply.github.com>
2025-02-17 07:11:10 +0000

    function

--- javascript/06_built_in_function.js
+++ javascript/06_built_in_function.js
@@ -25,3 +25,24 @@ console.log(parseInt("0x11", 16))
 // console.log(parseFloat("10.33"))
 // console.log(parseFloat("34 45 66"))
 // console.log(parseFloat("He was 40"))
+
+//map
+const arr = [1, 2, 3, 4, 5];
+const output = arr.map((num) => num += 10)
+console.log(arr); // [1, 2, 3, 4, 5]
+console.log(output); // [11, 12, 13, 14, 15]
+
+//filter
+const arr2 = [1, 2, 3, 4, 5];
+const output2 = arr.filter((num) => num % 2) // filter out odd numbers
+console.log(arr2); // [1, 2, 3, 4, 5]
+console.log(output2); // [1, 3, 5]
+
+//reduce
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = numbers.reduce((total, currentValue) => {
+    return total + currentValue;
+}, 0)
+
+console.log(sum); // 15
