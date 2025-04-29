badc65b81e724e8ecb858728eceffe933c853fac
me <dinuamipara611@gmail.com>
2025-04-29 16:52:22 +0530

    jest test

---
+++ test-jest/sum.test.js
@@ -0,0 +1,48 @@
+//+++++++++++number++++++++++++
+// const sum = require('./sum');
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+//++++++++++++String++++++++++++
+// test('there is no I in team', () => {
+//   expect('team').not.toMatch(/I/);
+// });
+
+// test('but there is a "stop" in Christoph', () => {
+//   expect('Christoph').toMatch(/stop/);
+// });
+
+//++++++++++++Array++++++++++++
+// const shoppingList = [
+//   'diapers',
+//   'kleenex',
+//   'trash bags',
+//   'paper towels',
+//   'milk',
+// ];
+
+// test('the shopping list has milk on it', () => {
+//   expect(shoppingList).toContain('milk');
+//   expect(new Set(shoppingList)).toContain('milk');
+// });
+
+
+//++++++++++Exception+++++++++++
+function compileAndroidCode() {
+  throw new Error('you are using the wrong JDK!');
+}
+
+test('compiling android goes as expected', () => {
+  expect(() => compileAndroidCode()).toThrow();
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  // You can also use a string that must be contained in the error message or a regexp
+  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
+
+  // Or you can match an exact error message using a regexp like below
+  expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK$/); // Test fails
+  expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK!$/); // Test pass
+});
